53bf4ee0603c3b485ac7f441349077a4b41ea556
nsihor <nsihor@gmail.com>
2024-09-17 14:26:51 +0300

    delete mobile portfolio modal

--- src/js/portfolioModal.js
+++ src/js/portfolioModal.js
@@ -1,2 +1,6 @@
-const exteriorsModal = new SimpleLightbox('.exteriors a', { captionDelay: 250 });
-const interiorsModal = new SimpleLightbox('.interiors a', {captionDelay: 250});
+const screenWidth = window.innerWidth;
+
+if (screenWidth > 1024) {
+    const exteriorsModal = new SimpleLightbox('.exteriors a', { captionDelay: 250 });
+    const interiorsModal = new SimpleLightbox('.interiors a', { captionDelay: 250 });
+}
