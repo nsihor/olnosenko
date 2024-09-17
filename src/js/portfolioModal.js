c3914617731bf646b97c77f3621fcc4fbbf32432
nsihor <nsihor@gmail.com>
2024-09-17 16:41:36 +0300

    made modal in full size

--- src/js/portfolioModal.js
+++ src/js/portfolioModal.js
@@ -1,6 +1,2 @@
-const screenWidth = window.innerWidth;
-
-if (screenWidth > 1024) {
-    const exteriorsModal = new SimpleLightbox('.exteriors a', { captionDelay: 250 });
-    const interiorsModal = new SimpleLightbox('.interiors a', { captionDelay: 250 });
-}
+const exteriorsModal = new SimpleLightbox('.exteriors a', { captionDelay: 250, heightRatio: 1, widthRatio: 1 });
+const interiorsModal = new SimpleLightbox('.interiors a', { captionDelay: 250, heightRatio: 1, widthRatio: 1 });
